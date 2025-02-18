ce6cc0fc7bf2396ce81e9545dfa06284ca526d6e
SungHyun Hong <69339846+hongsam14@users.noreply.github.com>
2025-02-18 15:49:36 +0900

    Dev: develop custom Exception

---
+++ NanoException/ConfigException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Nanolite_agent.NanoException
+{
+    public class ConfigException : Exception
+    {
+        public ConfigException() { }
+        public ConfigException(string message) : base(message) { }
+        public ConfigException(string message, Exception inner) : base(message, inner) { }
+    }
+}
